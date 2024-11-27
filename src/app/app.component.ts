e6f129a8eca343cee6c8715cc8dfb0b299f22ccb
Deepak <saisandeep3585@gmail.com>
2024-11-27 18:14:45 +0530

    minor change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,13 @@ import { ApiService } from './services/api.service';
 export class AppComponent implements OnInit{
   title = 'Payment-gateway';
   BankData : any;
-  constructor(private apiServe : ApiService){}
+  constructor(private apiServe : ApiService, private router : Router) { }
 
   ngOnInit(): void {
       this.apiServe.getBankData().subscribe((res : any)=>{
         let daattaa = this.apiServe.decryptData(res?.data)
         this.BankData = daattaa?.data;
+        this.router.navigate([`/bank/${this.BankData?.[2]?.bank_name}`])
       })
   }
    
